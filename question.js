89a2a8142f7f3e7339d8f0f9c0e289a4492397e1
sangavid8 <59510766+sangavid8@users.noreply.github.com>
2020-12-17 23:33:31 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,92 @@
+
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+
+    this.questionIndex++;
+}
+
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+
+
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
+
+
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
+
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+function showScores() {
+    var gameOverHTML = "<h1 id='h1'>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+
+// create questions here
+var questions = [
+    new Question("Which is the biggest continent in the world?", ["North America", "Asia","Europe", "Australia"], "Asia"),
+    new Question("Which is the biggest continent in the world?", ["Nile", "Amazon", "Niger", "Ganga"], "Nile"),
+    new Question("Which is India’s first super computer?", ["Param8000", "Param80000","Param800", "Param8"], "Param8000"),
+    new Question("Which is largest flower in the world?", ["Jasmine", "Rafflesia", "Balloon Flower", "Camellia"], "Rafflesia"),
+    new Question("Which is the 29th state of India?", ["Uttarakhand", "Telangana", "Uttar Pradesh", "Madhya Pradesh"], "Telangana")
+];
+
+// create quiz
+var quiz = new Quiz(questions);
+
+// display quiz
+populate();
